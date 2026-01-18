59dd965dbd55d888b5b64d8ecbd1b092a434e257
LuisPGuimaraes <luisegsilva.le@gmail.com>
2026-01-17 23:35:29 -0300

    feat: create principal page layout and components for counter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CountDisplay from './components/CountDisplay'
+import IncrementButton from './components/IncrementButton'
+import ResetButton from './components/ResetButton'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1 className="title">The Big Counter</h1>
+      <div className="counter-area">
+        <CountDisplay count={count} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="controls">
+        <ResetButton onReset={() => setCount(0)} />
+        <IncrementButton onIncrement={() => setCount((value) => value + 1)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
